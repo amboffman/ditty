8ed6ae733ad145984998c41a7f621ff013157bbb
Anthony Boffman <amarrokalboffman@gmail.com>
2020-12-21 14:08:54 -0800

    Build Spotify authorization

--- app/src/main/java/com/example/music_quiz/MainActivity.java
+++ app/src/main/java/com/example/music_quiz/MainActivity.java
@@ -2,13 +2,80 @@ package com.example.music_quiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+
+import com.spotify.protocol.client.Subscription;
+import com.spotify.protocol.types.PlayerState;
+import com.spotify.protocol.types.Track;
+
 
 public class MainActivity extends AppCompatActivity {
+    private static final String CLIENT_ID = "afca0c6d0ea04e77b84465e4c5d9f2f3";
+    private static final String REDIRECT_URI = "app://music.quiz";
+    private SpotifyAppRemote mSpotifyAppRemote;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Set the connection parameters
+        PackageManager pm = getPackageManager();
+        boolean isSpotifyInstalled;
+        try {
+            pm.getPackageInfo("com.spotify.music", 0);
+            isSpotifyInstalled = true;
+            Log.d("Spotify Installed", String.valueOf(isSpotifyInstalled));
+
+
+        } catch (PackageManager.NameNotFoundException e) {
+            isSpotifyInstalled = false;
+            Log.d("Spotify Not Installed", String.valueOf(isSpotifyInstalled));
+        }
+        ConnectionParams connectionParams =
+                new ConnectionParams.Builder(CLIENT_ID)
+                        .setRedirectUri(REDIRECT_URI)
+                        .showAuthView(true)
+                        .build();
+
+        SpotifyAppRemote.connect(this, connectionParams, new Connector.ConnectionListener(){
+            @Override
+            public void onConnected(SpotifyAppRemote spotifyAppRemote){
+                mSpotifyAppRemote = spotifyAppRemote;
+                Log.d("MainActivity", "Connected! Yay!");
+                // Now you can start interacting with App Remote
+                connected();
+            }
+            @Override
+            public void onFailure(Throwable throwable) {
+                Log.e("MainActivity", throwable.getMessage(), throwable);
+
+                // Something went wrong when attempting to connect! Handle errors here
+            }
+        });
+    }
+
+    private void connected() {
+        // Then we will write some more code here.
+        // Play a playlist
+        mSpotifyAppRemote.getPlayerApi().play("spotify:playlist:37i9dQZF1DX7K31D69s4M1");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // Aaand we will finish off here.
+    }
+
 }
