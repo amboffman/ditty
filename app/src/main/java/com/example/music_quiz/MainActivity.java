ef04ebb03969819d479d2101edc5dd3fa1bebccb
Anthony Boffman <amarrokalboffman@gmail.com>
2021-02-11 12:52:53 -0800

    Style main theme for rgadient background

--- app/src/main/java/com/example/music_quiz/MainActivity.java
+++ app/src/main/java/com/example/music_quiz/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.music_quiz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
@@ -21,6 +24,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().getDecorView()
+                .setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        ConstraintLayout layout = findViewById(R.id.main_layout);
+        layout.setBackgroundResource(R.drawable.gradient_animation);
+        AnimationDrawable animDrawable = (AnimationDrawable) layout.getBackground();
+        animDrawable.setEnterFadeDuration(10);
+        animDrawable.setExitFadeDuration(5000);
+        animDrawable.start();
         Button actionButton = (Button) findViewById(R.id.actionButton0);
         actionButton.setVisibility(View.GONE);
         Button actionButton2 = (Button) findViewById(R.id.actionButton1);
