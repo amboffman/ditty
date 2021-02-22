a624be4bb1c8ad16ee3b3a5e0d45e593343df3e4
Anthony Boffman <amarrokalboffman@gmail.com>
2021-02-22 15:31:41 -0800

    Fix playlist grid out of bound index error

--- app/src/main/java/com/example/music_quiz/PlaylistAdapter.java
+++ app/src/main/java/com/example/music_quiz/PlaylistAdapter.java
@@ -19,51 +19,6 @@ import com.spotify.protocol.types.ListItems;
 
 import java.util.ArrayList;
 
-//public class PlaylistAdapter extends BaseAdapter {
-//
-//    private Context context;
-//    private LayoutInflater inflater;
-//    private ArrayList title;
-//    private ArrayList imageUri;
-//
-//    public PlaylistAdapter(Context c, ArrayList title, ArrayList imageUri){
-//        context = c;
-//        this.title = title;
-//        this.imageUri = imageUri;
-//    }
-//    @Override
-//    public int getCount() {
-//        return title.size();
-//    }
-//
-//    @Override
-//    public Object getItem(int position) {
-//        return null;
-//    }
-//
-//    @Override
-//    public long getItemId(int position) {
-//        return 0;
-//    }
-//
-//    @Override
-//    public View getView(int position, View convertView, ViewGroup parent) {
-//        if(inflater == null){
-//            inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-//        }
-//        if(convertView == null){
-//            convertView = inflater.inflate(R.layout.row_item, null);
-//        }
-//        ImageView playlistImage = convertView.findViewById(R.id.playlistImage);
-//        TextView playlistTitle = convertView.findViewById(R.id.playlistTitle);
-//
-//        playlistImage.setImageResource((Integer) imageUri.get(position));
-//        playlistTitle.setText((Integer) title.get(position));
-//
-//        return convertView;
-//    }
-//}
-
 public class PlaylistAdapter extends ArrayAdapter{
 
     ListItems playlists;
@@ -90,10 +45,14 @@ public class PlaylistAdapter extends ArrayAdapter{
         }
         ImageView playlistImage = convertView.findViewById(R.id.playlistImage);
         TextView playlistTitle = convertView.findViewById(R.id.playlistTitle);
-        Log.d("Image Object", String.valueOf(playlistImages.get(position)));
-        playlistImage.setImageBitmap(playlistImages.get(position));
-        ListItem playlist = playlists.items[position];
-        playlistTitle.setText(playlist.title);
+            Log.d("Image Object position", String.valueOf(position));
+            Log.d("Image Object length", String.valueOf(playlists.items.length));
+        if(playlists.items.length > position) {
+            Log.d("Image Object", String.valueOf(playlistImages.get(position)));
+            playlistImage.setImageBitmap(playlistImages.get(position));
+            ListItem playlist = playlists.items[position];
+            playlistTitle.setText(playlist.title);
+        }
         return convertView;
     }
 }
